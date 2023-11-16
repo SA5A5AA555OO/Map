8915c39b19cdd90d7c7d71572e51b97a6599106b
hungyi0108 <eason910108@gmail.com>
2023-11-16 17:49:04 +0800

    1116 eason map ok version

--- src/screens/Map.js
+++ src/screens/Map.js
@@ -5,69 +5,72 @@ import {
     Text,
     Image,
     ScrollView,
-}from 'react-native';
+} from 'react-native';
+import MapView  from 'react-native-maps';
 
-const Map = ({navigation}) =>{
-   
+const Map = ({ navigation }) => {
+
     return (
-      <View style={styles.container}>
-        <ScrollView>
-          <Image
-            style={styles.logo}
-             source={require('map/asset/googlemap.jpg')}/>
-             <Text style={[styles.leftText, { alignSelf: 'flex-start' }]}>我的最愛</Text>
-             <Text style={[styles.leftText, { alignSelf: 'flex-start' }]}>訂單紀錄</Text>
-             <Text style={[styles.leftText, { alignSelf: 'flex-start' }]}>修改個人資訊</Text>
-        </ScrollView>  
-      </View>
-   
+        <View style={styles.container}>
+            <MapView
+            style={styles.map}
+                initialRegion={{
+                    latitude: 25.03719676448161,//經度
+                    longitude: 121.43248323862977,//緯度
+                    latitudeDelta: 0.01,
+                    longitudeDelta: 0.01,
+                }}
+            />
+        </View>
+
     );
 };
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'flex-start',
-    backgroundColor:'#FDFBF1',
-    paddingTop:20,
-  },
-  headerText: {
-      fontSize: 40,
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'flex-start',
+        backgroundColor: '#FDFBF1',
+        paddingTop: 20,
     },
-  logo: {
-    width: 400,
-    height: 800,
-    borderRadius: 30,
-      
+    headerText: {
+        fontSize: 0,
     },
-  leftText :{
-      fontSize: 25,
-      left:20,
-  },
-  detail:{
-      fontSize: 20,
-      left:20
-  },
-  link:{
-      color:'#DA7746',
-  },
+    logo: {
+        width: 400,
+        height: 800,
+        borderRadius: 30,
 
-  
+    },
+    leftText: {
+        fontSize: 25,
+        left: 20,
+    },
+    detail: {
+        fontSize: 20,
+        left: 20
+    },
+    link: {
+        color: '#DA7746',
+    },
+    map:{
+        width:'100%',
+        height:'100%',
+    },
 
-  
-  buttonContainer: {
-    backgroundColor: '#E6A984', // 自定义背景颜色
-    padding: 20,
-    borderRadius: 20, // 圆角效果
-    marginVertical: 10, // 设置垂直间距
-    width:'80%'
-  },
-  buttonText: {
-    color: 'white', // 文本颜色
-    fontWeight: 'bold',
-    textAlign: 'center', // 文本居中
-    
-  },
+    buttonContainer: {
+        backgroundColor: '#E6A984', // 自定义背景颜色
+        padding: 20,
+        borderRadius: 20, // 圆角效果
+        marginVertical: 10, // 设置垂直间距
+        width: '80%'
+    },
+    buttonText: {
+        color: 'white', // 文本颜色
+        fontWeight: 'bold',
+        textAlign: 'center', // 文本居中
+
+    },
 });
 
 export default Map;
