07add7e5ba23b1d98861e8868d185e72b3254ea0
rickandmorto <ruikewang2@gmail.com>
2024-03-07 16:38:01 +0800

    a

--- src/screens/Introduction.js
+++ src/screens/Introduction.js
@@ -1,11 +1,25 @@
-import {Text, TextInput, View,Button, TouchableOpacity, StyleSheet,Image} from 'react-native';
+import {Text, View, TouchableOpacity, StyleSheet,Image,} from 'react-native';
+import React, { useEffect, useState } from 'react';
 import HomeScreen from '../screens/HomeScreen';
 import Introduction2 from '../screens/Introduction2';
-
+import firestore from "@react-native-firebase/firestore"
 const Introduction = ({navigation}) => {
+
+  const[userData,setUserData] =useState()
+
+const getData = async() =>{
+ const userCollection =await  firestore().collection("user").doc("UBDEL1cF2GcLMCFaza7C").get()
+ console.log(userCollection.data())
+ setUserData(userCollection.data())
+}
+
+useEffect(() =>{
+    getData()
+  },[])
+
   return (
     <View style={styles.container}>
-      
+       
       <Text></Text>
       <Text></Text>
       <Text></Text>
@@ -17,6 +31,17 @@ const Introduction = ({navigation}) => {
       <TouchableOpacity onPress={() => navigation.navigate('Introduction2')}>
         <Text style={styles.link}>下一頁</Text>
       </TouchableOpacity>
+      {userData ? (
+      <>
+        <Text>Email: {userData.email}</Text>
+        <Text>Password: {userData.password}</Text>
+        <Text>Phone: {userData.phone}</Text>
+        <Text>User ID: {userData.user_ID}</Text>
+        <Text>Username: {userData.username}</Text>
+      </>
+    ) : (
+      <Text>Loading...</Text>
+    )}
     </View>
   );
 };
