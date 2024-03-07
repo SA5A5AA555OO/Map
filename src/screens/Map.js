3b67a0e9706d03cf053412cef2bafde0f6a0b655
hungyi0108 <eason910108@gmail.com>
2024-03-07 14:51:27 +0800

    0307 new version

--- src/screens/Map.js
+++ src/screens/Map.js
@@ -1,21 +1,62 @@
-import React from "react";
+import React, { useState } from "react";
 import {
     View, StyleSheet, Text, Image, ScrollView,
 } from 'react-native';
-import MapView,{Marker} from 'react-native-maps';
+import MapView, { Marker } from 'react-native-maps';
+import MapViewDirections from 'react-native-maps-directions';
 import FjuRef from '../screens/FjuRef';
 import Store from '../screens/Store';
 
+const CustomMarker = ({ coordinate, title, icon }) => (
+    <Marker coordinate={coordinate} title={title}>
+        <View style={{ alignItems: 'center' }}>
+            <Image source={icon} style={{ width: 30, height: 30 }} />
+        </View>
+    </Marker>
+);
+
 const Map = ({ navigation }) => {
-    const handleButtonPress1 = () => {
-        navigation.navigate('FjuRef');
-      };
-    const handleButtonPress2 = () => {
-        navigation.navigate('Store');
-      };
+    const markerCoordinate = { latitude: 25.0335130275009, longitude: 121.43384983277525 };
+    const markerTitle = '您的位置';
+    const customIcon = require('map/asset/location.png'); //您的位置 圖片
+
+    const [origin, setOrigin] = useState({ latitude: 25.0335130275009, longitude: 121.43384983277525 });
+    const [destination, setDestination] = useState(null);
+
+    const [distance, setDistance] = useState(null);
+    const [duration, setDuration] = useState(null);
+
+
+    const [markerslist, setMarkersList] = useState([
+        {
+            id: 1,
+            latitude: 25.036536324078433,//經度
+            longitude: 121.43183548002627,//緯度
+            title: '食享冰箱',
+            description: '點擊食享冰箱'
+        },
+        {
+            id: 2,
+            latitude: 25.042853691197738,//經度
+            longitude: 121.44725038057825,//緯度
+            title: '素食的店',
+            description: '點擊素食的店'
+        }
+    ])
+
+    const handleButtonPress = (markerId) => {
+        if (markerId === 1) {
+            navigation.navigate('FjuRef');
+            setDestination({ latitude: 25.036536324078433, longitude: 121.43183548002627 });
+        } else if (markerId === 2) {
+            navigation.navigate('Store');
+            setDestination({ latitude: 25.042853691197738, longitude: 121.44725038057825 });
+        }
+    };
+
     return (
         <View style={styles.container}>
-            <MapView             
+            <MapView
                 style={styles.map}
                 initialRegion={{
                     latitude: 25.03719676448161,//經度
@@ -23,20 +64,39 @@ const Map = ({ navigation }) => {
                     latitudeDelta: 0.01,
                     longitudeDelta: 0.01,
                 }}>
-                <Marker //標記地點
-                coordinate={{latitude: 25.036536324078433,  longitude: 121.43183548002627}}
-                title={'食享冰箱'}
-                description={'點擊食享冰箱'}
-                onPress={handleButtonPress1}
-                />
-                <Marker //標記地點
-                coordinate={{latitude: 25.042853691197738,  longitude: 121.44725038057825}}
-                title={'素食的店'}
-                description={'點擊素食的店'}
-                onPress={handleButtonPress2}
-                />
+                <CustomMarker coordinate={markerCoordinate} title={markerTitle} icon={customIcon} />
+                {
+                    markerslist.map((marker) => {
+                        return (
+                            <Marker //標記地點
+                                key={marker.id}
+                                coordinate={{ latitude: marker.latitude, longitude: marker.longitude }}
+                                title={marker.title}
+                                description={marker.description}
+                                onPress={() => handleButtonPress(marker.id)}
+                            />
+                        )
+                    })
+                }
+                {origin && destination != undefined ? (
+                    <MapViewDirections
+                        origin={origin}
+                        destination={destination}
+                        strokeColor="black"
+                        strokeWidth={4}
+                        mode="walking"
+                        onReady={(result) => {
+                            setDistance(result.distance);
+                            setDuration(result.duration);
+                            
+                            console.log(`Distance: ${result.distance} km`);
+                            console.log(`Duration: ${result.duration} min`);
+                        }}
+                        apikey="AIzaSyBARwrOhviGWEHN94EDPR0Ojy - YftRlljA"
+                    />
+                ) : null}
             </MapView>
-           
+
         </View>
 
     );
