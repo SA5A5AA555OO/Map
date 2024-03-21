2ddf7c14900430b6539f30037d70b2cba312a4ef
shihjung wang <shi7jung@gmail.com>
2024-03-22 04:45:25 +0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,34 @@
+import React from "react";
+import {Text, View} from 'react-native';
+import Navigation from "./src/components/Navigation";
+const firebaseConfig = {
+  apiKey: "AIzaSyBARwrOhviGWEHN94EDPR0Ojy-YftRlljA",
+authDomain: "sa5a5aa555oo.firebaseapp.com",
+databaseURL: "https://sa5a5aa555oo-default-rtdb.asia-southeast1.firebasedatabase.app",
+projectId: "sa5a5aa555oo",
+storageBucket: "sa5a5aa555oo.appspot.com",
+messagingSenderId: "602378113582",
+appId: "1:602378113582:web:c6b308cc039586506ec5bf",
+measurementId: "G-NS22NW5C8F"
+};
+
+
+const handleSignUp = async () => {
+    try {
+      await auth().createUserWithEmailAndPassword(email, password);
+      console.log('User account created & signed in!');
+      navigation.navigate('LoginScreen');
+    } catch (error) {
+      if (error.code === 'auth/email-already-in-use') {
+        console.log('That email address is already in use!');
+      } else if (error.code === 'auth/invalid-email') {
+        console.log('That email address is invalid!');
+      }
+      console.error(error);
+    }
+  };
+const App = () => {
+    return <Navigation/>;
+};
+
+export default App
